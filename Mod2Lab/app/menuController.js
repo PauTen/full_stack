3fbb5cb414e299e6b8b6531cc81ec209f106f144
PauTen <paulo_ten@outlook.es>
2019-03-09 12:49:40 +0100

    Added new module labs

--- Mod2Lab/app/menuController.js
+++ Mod2Lab/app/menuController.js
@@ -4,8 +4,11 @@ app.controller('menuController', [
     '$scope',
     function($scope){
         $scope.model = {title: 'Our Menu'};
-        $scope.changeMainDish = function(item) {
-            $scope.model.mainDish = item;
+        $scope.changeMainDish = function(name, price) {
+            $scope.model.mainDish = {
+                "name": name,
+                "price":price
+            };
         };
 
         $scope.$watch('model.mainDish', function (newValue, oldValue) {
