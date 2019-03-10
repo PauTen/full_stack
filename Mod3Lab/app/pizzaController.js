365cc91e17a6366e3aa17f2ef2c898f7e78f290d
PauTen <paulo_ten@outlook.es>
2019-03-10 11:57:34 +0100

    New module added

--- Mod3Lab/app/pizzaController.js
+++ Mod3Lab/app/pizzaController.js
@@ -4,9 +4,14 @@ app.controller('pizzaController', [
         $scope.model = { 
             title: 'Pizza Builder',
             availableToppings: ['Cheese', 'Pepperoni', 'Bacon', 'Pineapple', 'Sausage', 'Ham', 'Chicken', 'Mushrooms', 'Onion', 'Olives', 'Green Peppers'],
-            toppings: [] },
+            toppings: [],
+            showToppingAdded: false,
+            showNoResults: false},
         $scope.addTopping = function(topping){
-            $scope.model.toppings.push(topping)
+            $scope.model.toppings.push(topping);
+            $scope.model.search = null;
+            $scope.model.showToppingAdded = true;
+            $scope.filteredToppings = [];
         };
     }
 ]);
